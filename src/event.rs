e4e80c264125ec87333016975532f841e12d8215
Ivan Tolmachev <ivatolm@gmail.com>
2022-05-04 20:34:04 +0300

    Implement event channel structure

--- src/event.rs
+++ src/event.rs
@@ -1,11 +1,24 @@
+use std::sync::mpsc::{Sender, Receiver};
+
 pub struct Event {
   pub sender: String,
   pub title: String,
   pub data: Vec<String>
 }
 
+pub struct EventChannel {
+  pub tx: Sender<String>,
+  pub rx: Receiver<String>
+}
+
 impl Event {
   pub fn new(sender: String, title: String, data: Vec<String>) -> Self {
     Self { sender, title, data }
+  }
+}
+
+impl EventChannel {
+  pub fn new(tx: Sender<String>, rx: Receiver<String>) -> Self {
+    Self { tx, rx }
   }
 }
