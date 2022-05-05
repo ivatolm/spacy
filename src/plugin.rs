d85650c7afabf593935864855aab245fd085b138
Ivan Tolmachev <ivatolm@gmail.com>
2022-05-05 23:35:19 +0300

    Add event-handler to the plugin

--- src/plugin.rs
+++ src/plugin.rs
@@ -1,5 +1,6 @@
 use std::net::TcpListener;
 use std::io::Read;
+use std::process::Command;
 use std::sync::mpsc::Sender;
 use std::thread::{self, JoinHandle};
 
@@ -16,8 +17,31 @@ impl Plugin {
     Self { ec, source }
   }
 
-  pub fn start(self) {
+  pub fn start(self) -> Vec<JoinHandle<()>> {
+    let lbtx = self.ec.lbtx.clone();
+    let server_handle = thread::spawn(move || Self::server(32001, lbtx));
 
+    Command::new("python3")
+      .arg("-c")
+      .arg(&self.source)
+      .spawn()
+      .unwrap();
+
+    let event_handler_handle = thread::spawn(move || self.event_handler());
+
+    vec![server_handle, event_handler_handle]
+  }
+
+  fn event_handler(self) {
+    loop {
+      let event = self.ec.rx.recv().unwrap();
+
+      match event.sender() {
+        "plugin" => println!("EventHandler received event from 'plugin'"),
+        "main" => println!("EventHandler received event from 'main'"),
+        _ => unreachable!()
+      }
+    }
   }
 
   fn server(port: u16, tx: Sender<Event>) {
