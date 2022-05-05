fd2314a800df101e22187dd8cf5208a8ad159d47
Ivan Tolmachev <ivatolm@gmail.com>
2022-05-05 18:04:40 +0300

    Add server to the plugin

--- src/plugin.rs
+++ src/plugin.rs
@@ -1,4 +1,10 @@
-use crate::event::EventChannel;
+use std::net::TcpListener;
+use std::io::Read;
+use std::sync::mpsc::Sender;
+use std::thread::{self, JoinHandle};
+
+use crate::event::{EventChannel, Event};
+use crate::tools;
 
 pub struct Plugin {
   pub ec: EventChannel,
@@ -12,5 +18,23 @@ impl Plugin {
 
   pub fn start(self) {
 
+  }
+
+  fn server(port: u16, tx: Sender<Event>) {
+    let listener = TcpListener::bind((tools::local_ip(), port)).unwrap();
+    let (mut stream, _) = listener.accept().unwrap();
+
+    let mut buf = [0u8; 1024];
+    let size = stream.read(&mut buf).unwrap();
+
+    let data = String::from_utf8(buf[..size].to_vec()).unwrap();
+    let (cmd, args) = data.split_once(' ').unwrap();
+
+    let event = Event::new(
+      "plugin".to_string(),
+      cmd.to_string(),
+      vec![args.to_string()]);
+
+    tx.send(event).unwrap();
   }
 }
